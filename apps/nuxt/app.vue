968247dfbe68be5286fbc49eef6049df132d497b
bichikim <bichi@live.co.kr>
2023-06-15 14:32:53 +0900

    remove chakra

--- apps/nuxt/app.vue
+++ apps/nuxt/app.vue
@@ -1,10 +1,9 @@
 <template>
-  <chakra.div m="2">
+  <div>
     hello {{ counter }}
-    <NuxtPage />
-  </chakra.div>
+    <nuxt-page />
+  </div>
 </template>
 <script lang="ts" setup>
-  import {chakra} from '@chakra-ui/vue-next'
   const counter = useState('counter', () => Math.round(Math.random() * 1000))
 </script>
--- apps/nuxt/pages/index.vue
+++ apps/nuxt/pages/index.vue
@@ -3,21 +3,6 @@
 </template>
 
 <script setup lang="ts">
-  import {
-    CBadge,
-    CButton,
-    CButtonGroup,
-    CCircle,
-    chakra,
-    CHeading,
-    CIconButton,
-    CStack,
-    CWrap,
-    CWrapItem,
-    useColorMode,
-    useColorModeValue,
-    useTheme,
-  } from '@chakra-ui/vue-next'
   const props = defineProps({})
   const emit = defineEmits({})
 </script>
