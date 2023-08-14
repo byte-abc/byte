ce6d53a5469804ec36e8875b5adf27c0182f3818
bichikim <bichi@live.co.kr>
2023-08-14 11:01:41 +0900

    update

--- apps/nuxt/server/schema.ts
+++ apps/nuxt/server/schema.ts
@@ -8,7 +8,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const createSchema = (root = '') => {
   return buildSchema({
-    // ./.nuxt/
+    // root 가 '' 이며 nuxt 시스템 내에서 호출 될때 ./.nuxt/schema.graphql 에 파일이 생성 됩니다
+    // graphql:generate script 과 같이 독립 실행일 경우 __dirname 는 ./server 입니다
     emitSchemaFile: resolve(__dirname, '../', root, 'schema.graphql'),
     resolvers: [HelloResolver],
   })
